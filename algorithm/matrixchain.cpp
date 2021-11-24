e28dab374d7c115f5b72fa8449e3d653b6a65432
kunmonster <260124226@qq.com>
2021-11-24 02:21:17 +0000

    addgenerate

--- algorithm/matrixchain.cpp
+++ algorithm/matrixchain.cpp
@@ -5,27 +5,49 @@
 */
 
 #include<iostream>
-
+#include<time.h>
 using namespace std;
 
 
+void generate_matrix(const int , int *);
 void matrix_mutiplie_iteraion(int * , int ** ,int **,int );
 void track_brackets(int , int , int **);
 
 int main(){
-    int  arr[] ={30,35,15,5,10,20,25};
-    int ** min = new int * [7];
-    int ** s = new int * [7];
-    for(int i=0;i<7;i++){
-        min[i] = new int[7];
-        s[i] = new int[7];
+    cout<<"请输入矩阵个数:";
+    int num=0;
+    cin>>num;
+
+    int i;
+    int * arr;
+    int ** min = new int *[num+1];
+    int ** s = new int * [num+1];
+    for(i=0;i<num+1;i++){
+        min[i] = new int[num+1];
+        s[i] = new int[num+1];
     }
-    matrix_mutiplie_iteraion(arr,min,s,7);
-    track_brackets(1,6,s);
+    generate_matrix(num+1,arr);
+    matrix_mutiplie_iteraion(arr,min,s,num+1);
+    track_brackets(1,num,s);
     system("pause");
     return 0;
 }
 
+/**
+*生成随机矩阵序列
+*paramters : 矩阵数组长度 , 矩阵数组
+*/
+void generate_matrix(const int num,int * matrix){
+    if(!num){
+        cout<<"矩阵数量不正确"<<endl;
+        exit(1);
+    }
+    matrix = new int[num];
+    srand(time(0));
+    for(int i=0;i<num;i++){
+        matrix[i] = rand() % 10 + 1;
+    }
+}
 /*
 *迭代方式计算矩阵连乘法
 *paramter: 矩阵数组，最少乘法次数存放，断开点,数组长度
@@ -55,6 +77,10 @@ void matrix_mutiplie_iteraion(int * matrix_arr,int ** min,int ** s,int len){
         }
     }
 }
+
+/*
+*paramters : 起点矩阵 ， 终点矩阵 ， 断开点存放数组
+**/
 void track_brackets(int left,int right,int ** s){
       if(left == right) return;
       track_brackets(left,s[left][right],s);
