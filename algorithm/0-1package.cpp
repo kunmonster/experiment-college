ff72778dfdd0845567216e26fbc62d9116f72831
kunmonster <260124226@qq.com>
2021-12-05 17:15:17 +0800

    finish_01knapsack

--- algorithm/0-1package.cpp
+++ algorithm/0-1package.cpp
@@ -1,45 +1,65 @@
+#include <algorithm>
 #include <iostream>
-#include <queue>
 #include <vector>
 
 using namespace std;
 
-typedef pair<int, int> good;
-typedef pair<int, double> good_average;
+typedef struct _GOOD_ {
+  /* data */
+  int weight;
+  int value;
+  double average_value;
+} good;
 
 struct cmp {
-  bool operator()(good_average a, good_average b) {
-    return a.second < b.second;
+  bool operator()(const good a, const good b) {
+    return a.average_value > b.average_value;
   }
 };
+
+void sort(good* a, int n) {
+  int i, j;
+  bool flag = false;
+  for (i = 0; i < n - 1; i++) {
+    for (j = 0; j < n - 1 - i; j++) {
+      if (a[j].average_value < a[j + 1].average_value) {
+        good temp = a[i];
+        a[j] = a[j+1];
+        a[j+1] = temp;
+        flag = true;
+      }
+    }
+    if (!flag) break;
+  }
+}
 
 class knapsack {
  private:
   //上界函数
   double bound(int i);
-  vector<good>* good_arr;  //重量数组
-  bool* res;               //结果数组
-  int num;                 //物品数量
-  int capacity;            //背包容量
-  int best_value;          //最大价值
-  int current_weight;      //当前背包重量
-  int current_value;       //当前背包价值
+  good* good_arr;      //重量数组
+  bool* res;           //结果数组
+  int num;             //物品数量
+  int capacity;        //背包容量
+  int best_value;      //最大价值
+  int current_weight;  //当前背包重量
+  int current_value;   //当前背包价值
 
  public:
-  knapsack(vector<good>*, int);
+  knapsack(good*, int, int);
   void getBestMatch(int);
   void getResult();
   ~knapsack();
 };
 
-knapsack::knapsack(vector<good>* good_arr, int capacity) {
-  this->good_arr = new vector<good>();
+knapsack::knapsack(good* good_arr, int num, int capacity) {
+  this->good_arr = new good[num];
   this->good_arr = good_arr;
   this->capacity = capacity;
   this->current_value = 0;
   this->current_weight = 0;
   this->best_value = 0;
-  this->num = this->good_arr->size();
+  this->num = num;
   this->res = new bool[this->num]{false};
 }
 
@@ -48,81 +68,80 @@ knapsack::~knapsack(){};
 double knapsack::bound(int index) {
   if (!this->good_arr) return 0;
 
-  priority_queue<good_average, vector<good_average>, cmp> pri_que;
-  for (int i = index; i < this->num; i++) {
-    good temp = this->good_arr->at(i);
-    pri_que.push(
-        pair<int, double>(temp.first, temp.second / (double)temp.first));
-  }
   //
-  int c = this->capacity;         //当前容量
-  int cw = this->current_weight;  //当前重量
+  int left_c = this->capacity - this->current_weight;
   double cv = this->current_value;
-  while (!pri_que.empty() && c >= 0) {
-    good_average temp = pri_que.top();
-    if (temp.first + cw <= c) {
-      //该物品能装
-      //装入
-      cw += temp.first;
-      c -= temp.first;
-      cv += temp.first * temp.second;
-    } else {
-      //该物品重量加上过后超过了背包的容量
-      //对该物品分割
-      int sub = c - cw;
-      cv += sub * temp.second;
-      c = this->capacity;
-      break;
-    }
+
+  int i;
+  int len = num - index;
+  good temp[len];
+  for (i = 0; i < len; i++) {
+    temp[i] = good_arr[i + index];
   }
+  sort(temp, len);
+  i = 0;
+  while (i < len && temp[i].weight <= left_c) {
+    left_c -= temp[i].weight;
+    cv += temp[i].value;
+    i++;
+  }
+  if (i < len) {
+    cv += left_c * temp[i].average_value;
+    left_c = 0;
+  }
+
   return cv;
 }
 void knapsack::getBestMatch(int index) {
   //回溯
-  if (index >= this->num) {
+  if (index > this->num - 1) {
     this->best_value = this->current_value;
     this->res[index] = true;
     return;
   }
-  if ((this->current_weight + this->good_arr->at(index).first) <=
-      this->capacity) {
+  if ((this->current_weight + this->good_arr[index].weight) <= this->capacity) {
     //左子树
-    current_value += this->good_arr->at(index).second;
-    current_weight += this->good_arr->at(index).first;
+    current_value += this->good_arr[index].value;
+    current_weight += this->good_arr[index].weight;
     res[index] = true;
     getBestMatch(index + 1);
-    current_value -= this->good_arr->at(index).second;
-    current_weight -= this->good_arr->at(index).first;
+    current_value -= this->good_arr[index].value;
+    current_weight -= this->good_arr[index].weight;
   }
-  int bound_c = this->bound(index + 1);
+  double bound_c = this->bound(index + 1);
   if (bound_c > best_value) {
-    res[index] = false;
+    this->res[index] = false;
     getBestMatch(index + 1);
   }
 }
 
 void knapsack::getResult() {
   if (this->res) {
+    cout << "装入背包最大价值为:" << this->best_value << endl;
     int size = this->num;
+    cout << "装入背包的物品序号为:" << endl;
     for (int i = 0; i < size; i++) {
-      if(res[i]){
-        cout<<i<<"\t";
+      if (res[i]) {
+        cout << i << "\t";
       }
     }
   }
 }
 
 int main() {
+  cout << "请输入物品个数,背包容量 : [物品个数] [容量]" << endl;
   int n, c;
   cin >> n >> c;
-  vector<good> good_arr;
-  good_arr.resize(n);
+  good* good_arr = new good[n];
   int weight, value;
+  cout << "请输入每个物品的重量和价值 : [重量] [价值]" << endl;
   for (int i = 0; i < n; i++) {
     cin >> weight >> value;
-    good_arr[i] = pair<int, int>(weight, value);
+    good_arr[i].weight = weight;
+    good_arr[i].value = value;
+    good_arr[i].average_value = value / (double)weight;
   }
-  knapsack* sol = new knapsack(&good_arr, c);
+  knapsack* sol = new knapsack(good_arr, n, c);
   sol->getBestMatch(0);
   sol->getResult();
   return 0;
