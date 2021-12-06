518f21c90a75e631b06bcc0114783613ee761898
kunmonster <260124226@qq.com>
2021-12-06 16:37:35 +0800

    nqueen

--- algorithm/nQueen.cpp
+++ algorithm/nQueen.cpp
@@ -1,43 +1,55 @@
 /**
-*fkj
-*2021/12/05
-*回溯n后
-*/
-#include<iostream>
+ *fkj
+ *2021/12/05
+ *回溯n后
+ */
+#include <iostream>
 using namespace std;
 
-class Nqueen{
-    private:
-    bool place(int );      //限界函数
-    int n;          //棋盘的行列数
-    int * res;      //结果数组，res[i]表示第i行存放的列数
-    public:
-    Nqueen(int);
-    ~Nqueen();
-    void BackTrack(int);
+class Nqueen {
+ private:
+  bool place(int);  //限界函数
+  int n;            //棋盘的行列数
+  int* res;         //结果数组，res[i]表示第i行存放的列数
+  int sum;
+ public:
+  Nqueen(int);
+  ~Nqueen();
+  void BackTrack(int);
 };
 
-Nqueen::Nqueen(int n){
-    this->n = n;
-    this->res = new int[n+1]{0};
+Nqueen::Nqueen(int n) {
+  this->n = n;
+  this->res = new int[n + 1]{0};
+  this->sum=0;
 }
-Nqueen::~Nqueen(){}
+Nqueen::~Nqueen() { cout << "析构调用"; }
 
-bool Nqueen::place(int index){
+bool Nqueen::place(int index) {
+  for (int i = 1; i < index; i++) {
+    if (abs(index - i) == abs(this->res[i] - this->res[index]) ||
+        (this->res[i] == this->res[index]))
+      return false;
+  }
+  return true;
 }
-void Nqueen::BackTrack(int index){
-if(index > this->n) {
+void Nqueen::BackTrack(int index) {
+  if (index > this->n) {
+    this->sum++;
     return;
-}
-else{
-    for(int i=1;i<=this->n;i++){
-        res[index] = i;
-        if(place(index)){
-            BackTrack(index+1);
-        } 
+  } else {
+    for (int i = 1; i <= this->n; i++) {
+      res[index] = i;
+      if (place(index)) {
+        BackTrack(index + 1);
+      }
     }
+  }
 }
-}
-int main(){
-    return 0;
+int main() {
+  int a;
+  cin >> a;
+  Nqueen* nqueen = new Nqueen(a);
+  nqueen->BackTrack(1);
+  return 0;
 }
