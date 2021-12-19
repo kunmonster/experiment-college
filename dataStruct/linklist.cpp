72b795bab796263f47fcf810bacd3572d238471d
kunmonster <260124226@qq.com>
2021-12-19 18:30:32 +0800

    linklist

--- dataStruct/linklist.cpp
+++ dataStruct/linklist.cpp
@@ -1,29 +1,48 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 template <typename T>
 
-typedef struct _NODE_{
-    T data;
-    struct _NODE_ * next;
-    _NODE_(T data , struct _NODE_ * next=NULL) : data(data) , next(next){}
-}node;
+class Node {
+ private:
+  T data;
+  Node* next;
 
-node * createLinkList();
-bool insertLinkNode(int,node,node *);
-bool removeLinkNode(int,node *);
-T getDataByIndex(int);
-int getSize(node *);
+ public:
+  Node(T data = NULL, Node* next = NULL) : data(data), next(next) {}
+};
 
-int main(){
-    return 0;
+template <typename T>
+class LinkList {
+ private:
+  Node<T>* head;
+  int len;
+
+ public:
+  //初始化构造
+  LinkList() : head(new Node<T>*()), len(0) {}
+  //拷贝构造
+  LinkList(Node<T>* node);
+  //创建链表
+  void createLinkList();
+  //插入元素,index为索引
+  bool insertNode(int index, Node<T>* node);
+  //删除链表第index个元素
+  bool removeNode(int index);
+  //通过索引获取元素
+  T getDataByIndex(int index);
+};
+template <typename T>
+void LinkList<T>::createLinkList() {
+  T data;
+  Node<T>* temp = head;
+  while (cin >> data) {
+      
+  }
 }
 
-node * createLinkList(){
-    node * head = new node();
-    T data;
-    while(std::cin>>data){
-        node * temp = new node(data,NULL);
-        head->next = temp;
-    }
+int main() {
+  LinkList<char>* link = new LinkList<char>();
+  link->createLinkList();
+  return 0;
 }
