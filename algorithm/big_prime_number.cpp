380966f7c3d47cd5aff1a37c6a38351054b979a4
kunmonster <260124226@qq.com>
2021-12-20 21:10:30 +0800

    big_prime

--- algorithm/big_prime_number.cpp
+++ algorithm/big_prime_number.cpp
@@ -1,7 +1,7 @@
 
-#include <rand.h>
-
 #include <iostream>
+
+#include "./rand.h"
 
 using namespace std;
 
@@ -14,15 +14,15 @@ LL gcd(LL a, LL b) { return b ? gcd(b, a % b) : a; }
 /**
  * 快速乘法
  */
-LL qucik_multiply(LL a, LL b) {
+LL qucik_multiply(LL a, LL b, LL p) {
   //快速乘,转化为二进制,再相加
   LL ans = 0;
   while (b) {
     if (b & 1) {
-      ans += a;
+      ans = (ans + a) % p;
     }
-    a += a;
     b >>= 1;
+    a = (a + a) % p;
   }
   return ans;
 }
@@ -30,41 +30,53 @@ LL qucik_multiply(LL a, LL b) {
  * 快速幂
  *
  */
-LL quick_power(LL a, LL b) {
+LL quick_power(LL a, LL b, LL p) {
   LL ans = 1;
   while (b) {
     if (b & 1) {
-      ans *= a;
+      ans = (ans * a) % p;
     }
     b >>= 1;
-    a *= a;
+    a = (a * a) % p;
   }
   return ans;
 }
 
-bool Miller_Rabbin(LL n) {
+bool Miller_Rabin(LL n) {
   //判断是否为素数
   if (n == 2) return true;
   if (n < 2 || !(n & 1)) return false;
   LL m = n - 1;
   int k = 0;
   while ((m & 1) == 0) {
-    k++;
-    m >> 1;
+    k++;     //为t
+    m >>= 1;  //为k
   }
+  //费马小定律
+  // a^p-1 = 1(mod p)
+  // 10为产生的a的个数
   for (int i = 0; i < 10; i++) {
-    LL a = randnumber.generateRand(n - 1);
-    LL x = quick_power(a, m);
+    LL a = randnumber.generateRand(n - 1) + 1;
+    LL x = quick_power(a, m, n);
     LL y = 0;
-    for(int j=0;i<k;j++){
-      y = qucik_multiply(x,x);
-      if(y == 1 && x!=1 && x!=n-1) return false;
-      x=y;
+    for (int j = 0; j < k; j++) {
+      y = qucik_multiply(x, x, n);
+      if (y == 1 && x != 1 && x != n - 1) return false;
+      x = y;
     }
-    if(y!=1) return false;
+    if (y != 1) return false;
   }
   return true;
 }
 
-
-int main() { return 0; }
+int main() {
+  LL n;
+  while (cin >> n) {
+    if (Miller_Rabin(n))
+      printf("是素数\n");
+    else
+      printf("不是素数\n");
+  }
+  return 0;
+  return 0;
+}
