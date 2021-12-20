4f40637516853d3cf4a5c2011725259ac0bd8501
kunmonster <260124226@qq.com>
2021-12-20 09:29:55 +0800

    ssns#

--- algorithm/rand.cpp
+++ algorithm/rand.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
 #include<time.h>
 
+const unsigned long maxshort = 65536L;
+const unsigned long multiplier = 1194211693L;
+const unsigned long adder  = 12345L;
+
 class Rand{
     private:
     unsigned long randSeed;
     public:
-    Rand(unsigned long);        //constructor
-    unsigned short generateRand(unsigned long seed=0);       //get the number from 0 to n-1
+    Rand(unsigned long seed=0);        //constructor
+    unsigned short generateRand(unsigned long n);       //get the number from 0 to n-1
     double generateRand_1();                          //get the number from 0 to 1
 };
 Rand::Rand(unsigned long seed){
@@ -14,4 +18,22 @@ Rand::Rand(unsigned long seed){
         this->randSeed = time(0);
     else
         this->randSeed = seed;
+}
+unsigned short Rand::generateRand(unsigned long n){
+  this->randSeed = multiplier * this->randSeed + adder;
+  return (unsigned short)((this->randSeed>>16)%n);
+}
+
+double Rand::generateRand_1(){
+  return this->generateRand(maxshort) / (double)maxshort;
+}
+
+int main(){
+  Rand rand;
+  int a = rand.generateRand(5);
+  int arr[100]{0};
+  for(int i=0;i<=99;i++){
+    arr[i]=rand.generateRand(5);
+  }
+  return 0;
 }
