92fd6c4eee7bccd5c0a88f2a6d24494fa7eae548
kunmonster <260124226@qq.com>
2022-05-24 21:27:58 +0800

    almost finish

--- dataStruct/threadedBinaryTree.cc
+++ dataStruct/threadedBinaryTree.cc
@@ -26,11 +26,6 @@ typedef struct TreeNode {
   }
 } * ThreadTree;
 
-void PreOrder(const TreeNode* origin, TreeNode* newroot) {
-  if (origin) {
-  }
-}
-
 struct TreeNode* copyTree(const TreeNode* root) {
   TreeNode* res;
   while (root) {
@@ -124,6 +119,99 @@ void Pre(ThreadTree& root, ThreadTree& pre) {
   pre->m_rtag = true;
   pre = nullptr;
 }
+//遍历前序线索二叉树
+void PreOrder(const TreeNode* root) {
+  auto cur = root;
+  while (cur) {
+    cout << cur->m_data << "\t";
+    if (cur->m_ltag == false) {
+      cur = cur->m_lchild;
+    } else
+      cur = cur->m_rchild;
+  }
+}
+
+//中序线索树的第一个结点
+TreeNode* Mid_Firstnode(ThreadTree& root) {
+  while (root->m_ltag == false) {
+    root = root->m_lchild;
+  }
+  return root;
+}
+//中序线索树的下一个结点
+TreeNode* Mid_Nextnode(ThreadTree& root) {
+  if (root->m_rtag == false) return Mid_Firstnode(root->m_rchild);
+  return root->m_rchild;
+}
+
+// the last node of mid_thread
+TreeNode* Mid_lastnode(ThreadTree& root) {
+  //最右结点
+  while (root->m_rtag == false) {
+    root = root->m_rchild;
+  }
+  return root;
+}
+
+// the pre_node of tree_node
+TreeNode* Mid_prenode(TreeNode* node) {
+  if (node->m_ltag == false) return Mid_Firstnode(node->m_lchild);
+  return node->m_lchild;
+}
+
+void Mid_travel(ThreadTree& root) {
+  //
+  cout << "中序线索树中序遍历:";
+  auto cur = Mid_Firstnode(root);
+  while (cur) {
+    cout << cur->m_data << "\t";
+    cur = Mid_Nextnode(cur);
+  }
+}
+
+//前序线索树中结点的后继结点
+TreeNode* Pre_nextnode(TreeNode* node) {
+  if (node->m_ltag == false) return node->m_lchild;
+  return node->m_rchild;
+}
+
+//只有有头结点的前序线索树才能完美得到前驱，否则可能都不到前驱
+// TODO:加入dummy结点(虚拟头结点)完成相应功能
+// TreeNode * Pre_prenode(TreeNode * node){
+// }
+
+//前序线索树的最后一个结点
+TreeNode* Pre_lastnode(ThreadTree& root) {
+  while (root->m_rtag == false) {
+    root = root->m_rchild;
+  }
+  return root;
+}
+
+void Pre_travel(ThreadTree& root) {
+  cout << "前序线索树遍历:";
+  while (root) {
+    cout << root->m_data << "\t";
+    root = Pre_nextnode(root);
+  }
+}
+
+//后序线索树第一个结点
+TreeNode* Post_firstnode(ThreadTree& root) {
+  while(root->m_ltag == false) root = root->m_lchild;
+  if (root->m_lchild == nullptr && root->m_rtag == false)
+    return Post_firstnode(root->m_rchild);
+    return root;
+}
+
+//后序线索树后继结点
+//此操作需要直到双亲结点
+//1.当结点为根节点时，后继为空
+//2.当结点为双亲右孩子，或者为双亲左孩子且双亲无右孩子，那么后继为双亲
+//3.当结点为左孩子结点且双亲右孩子结点，那么后继为双亲右子树的后序第一个结点
+// TreeNode * Post_nextnode(TreeNode * node){
+
+// }
 
 int main() {
   //测试用例
@@ -131,6 +219,8 @@ int main() {
   ThreadTree root;
   CreateTree(root);
   ThreadTree pre = new TreeNode();
+  Pre(root, pre);
+  Pre_travel(root);
   // MidThreadTree(root, pre);
   return 0;
 }
