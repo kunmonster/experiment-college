e65e054112368124866e983004b8719dc9cb02c5
kunmonster <260124226@qq.com>
2022-07-05 22:40:47 +0800

    add binary_insert_sort==>next_shell

--- dataStruct/insert_sort.cc
+++ dataStruct/insert_sort.cc
@@ -1,73 +1,106 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T>
-class Element
-{
+template <class T>
+class Element;
 
-private:
-    T m_data;
+template <class T>
+std::ostream &operator<<(std::ostream &, const Element<T> &);
 
-public:
-    Element(T p_data=NULL) : m_data(p_data) {}
-    bool operator<(const Element &a)
-    {
-        return this->m_data < a.m_data;
-    }
-    bool operator == (const Element &a)
-    {
-        return this->m_data == a.m_data;
-    }
-    bool operator>(const Element &a)
-    {
-        return this->m_data > a.m_data;
-    }
-    bool operator <= (const Element & a){
-        return this->m_data <= a.m_data;
-    }
-    bool operator >= (const Element & a){
-        return this->m_data >= a.m_data;
-    }
-    friend ostream & operator <<(ostream & os,Element & a);
+template <class T>
+class Element {
+  friend std::ostream &operator<< <>(std::ostream &, const Element<T> &);
 
+ private:
+  T m_data;
+
+ public:
+  Element(){};
+  Element(T p_data) : m_data(p_data) {}
+  bool operator<(const Element &a) { return this->m_data < a.m_data; }
+  bool operator==(const Element &a) { return this->m_data == a.m_data; }
+  bool operator>(const Element &a) { return this->m_data > a.m_data; }
+  bool operator<=(const Element &a) { return this->m_data <= a.m_data; }
+  bool operator>=(const Element &a) { return this->m_data >= a.m_data; }
 };
-template<typename T>
-ostream & operator <<(ostream & os, const Element<T>& a){
-        os<<a.m_data;
-        return os;
-    }
-
-/***
- * 直接插入排序,升序
- * para : 数组,数组长度
- * **/
-template<typename T>
-void stright_insert_sort(Element<T> *arr, int len)
-{
-    if (len <= 1)
-        return;
-    // set the first element as the standard
-    Element<T> guard;
-    int j = 0;
-    for (int i = 1; i < len; i++)
-    {
-        if (*(arr + i) < *(arr + i - 1))
-        {
-            //当前元素小于前一个元素
-            guard = *(arr + i);
-            for (j = (i - 1); j >= 0 && arr[j] >= arr[i]; --j)
-                arr[j + 1] = arr[j];
-            arr[j + 1] = guard;
-        }
-    }
+template <class T>
+std::ostream &operator<<(std::ostream &os, const Element<T> &a) {
+  os << a.m_data;
+  return os;
 }
 
-int main()
-{
-    Element<int> *arr = new Element<int>[10]{9, 1, 3, 4, 5, 6, 5, 1, 6, 7};
-    stright_insert_sort(arr, 10);
-    for(int i=0;i<9;i++){
-        cout<<*(arr+i);
+/**
+ * @brief 直接插入排序(升序)
+ * @tparam T
+ * @param arr
+ * @param len
+ */
+template <typename T>
+void stright_insert_sort(Element<T> *arr, int len) {
+  if (len <= 1) return;
+  // set the first element as the standard
+  Element<T> guard;
+  int j = 0;
+  for (int i = 1; i < len; ++i) {
+    if (*(arr + i) < *(arr + i - 1)) {
+      //当前元素小于前一个元素
+      guard = *(arr + i);
+      for (j = (i - 1); (j >= 0) && (guard < arr[j]); --j) arr[j + 1] = arr[j];
+      arr[j + 1] = guard;
     }
-    return 0;
+  }
+}
+
+/**
+ * @brief  二分插入排序
+ * @tparam T
+ * @param arr
+ * @param len
+ */
+template <typename T>
+void binary_insert_sort(Element<T> *arr, int len) {
+  if (len <= 1) return;
+  //数组长度大于等于1
+
+  Element<T> guard;
+  int i, j;
+  for (i = 1; i < len; ++i) {
+    if (arr[i] < arr[i - 1]) {
+      guard = arr[i];
+      //得到插入的位置然后插入
+      int low = 0, high = i - 1, mid = 0;
+      while (low <= high) {
+        mid = (low + high) / 2;
+        if (guard < arr[mid])
+          high = mid - 1;
+        else
+          low = mid + 1;
+      }
+      for (j = (i - 1); j >= (high + 1); --j) arr[j + 1] = arr[j];
+      arr[high + 1] = guard;
+    }
+  }
+}
+
+/**
+ * @brief  希尔排序
+ * @tparam T 
+ * @param arr 
+ * @param len 
+ */
+template<typename T>
+void shell_sort(Element<T> * arr , int len){
+
+}
+
+int main() {
+  Element<char> *arr = new Element<char>[10] {
+    'A', 'C', 'a', 'C', 'b', 't', 'f', 'B', 'X', 'e'
+  };
+  binary_insert_sort(arr, 10);
+  //   stright_insert_sort(arr, 10);
+  for (int i = 0; i <= 9; i++) {
+    cout << *(arr + i);
+  }
+  return 0;
 }
